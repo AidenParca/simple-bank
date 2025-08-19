1d6ff3f5fbf8fb9d778af9f7c98ca87d6b12f2d1
AidenParca <parsamohammadi1@gmail.com>
2025-08-19 19:25:28 +0000

    Deadlock possibility fixed, TestTransferTxDeadlock Added

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -8,6 +8,56 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+
+	n := 10
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAcountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAcountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		go func() {
+			_, err := store.transferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAcountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errs <- err
+
+		}()
+
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	updateAccount1, err := testQouries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updateAccount2, err := testQouries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> After:", account1.Balance, account2.Balance)
+	require.Equal(t, account1.Balance, updateAccount1.Balance)
+	require.Equal(t, account2.Balance, updateAccount2.Balance)
+}
+
 func TestTransferTx(t *testing.T) {
 	store := NewStore(testDB)
 
